3fb181929bbbe3718f3719a0eba04011feb4ce5c
Finka Ramadhani <finkaramadhani99@gmail.com>
2022-09-24 14:13:49 +0700

    sayang iyop

---
+++ app/Views/header.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="header.css">
+
+</head>
+
+<body>
+    <div class="container-main">
+        <div class="row">
+            <div class="col-6">
+                <img class="ukuran-logo" src="http://1.bp.blogspot.com/-ZVv0pkxHftc/VKlBf-ZrnXI/AAAAAAAABOk/C3vVyWcyKjg/s1600/ukm-bs%2Bpolinela.png">
+            </div>
+            <div class="col-6">
+                <nav class="navbar navbar-expand-lg">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse " id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a href="/" class="nav-link tulisan-menu active" aria-current="page">Beranda</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/profile" class="nav-link tulisan-menu">Profil</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tulisan-menu">Berita</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tulisan-menu disabled">Galeri</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tulisan-menu menu-kontak">Kontak</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div class="container-title2">
+                    <div class="container-title">
+                        <div class="container-text">
+                            <h1 class="title">Hallo, Selamat Datang Di UKM Bidang Seni</h1>
+                            <p class="content">Ini isinya...</p>
+                            <input type="text" class="search-input" placeholder="cari">
+                        </div>
+                    </div>
+                    <img class="gambar" src="https://cdn3.vectorstock.com/i/1000x1000/76/32/doodle-seamless-background-school-pattern-vector-21587632.jpg">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h3 class="berita">Berita Terbaru</h3>
+                <div class="garis"></div>
+            </div>
+        </div>
+        <div class="row jarak-atas">
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://disk.mediaindonesia.com/thumbs/1800x1200/news/2019/09/68a51950c504564143258161fc5a3ebc.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <div class="container-card-info">
+                            <p class="card-info-category">kesenian</p>
+                            <p class="card-info-date">01 May 2022</p>
+                        </div>
+                        <h5 class="card-title">Unit Kegiatan Mahasiswa Bidang Seni (UKMBS) Menggelar Festival Band Se Lampung</h5>
+                        <a href="#">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://disk.mediaindonesia.com/thumbs/1800x1200/news/2019/09/68a51950c504564143258161fc5a3ebc.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <div class="container-card-info">
+                            <p class="card-info-category">kesenian</p>
+                            <p class="card-info-date">01 May 2022</p>
+                        </div>
+                        <h5 class="card-title">Unit Kegiatan Mahasiswa Bidang Seni (UKMBS) Menggelar Festival Band Se Lampung</h5>
+                        <a href="#">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://disk.mediaindonesia.com/thumbs/1800x1200/news/2019/09/68a51950c504564143258161fc5a3ebc.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <div class="container-card-info">
+                            <p class="card-info-category">kesenian</p>
+                            <p class="card-info-date">01 May 2022</p>
+                        </div>
+                        <h5 class="card-title">Unit Kegiatan Mahasiswa Bidang Seni (UKMBS) Menggelar Festival Band Se Lampung</h5>
+                        <a href="#">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 center">
+                <button class="btn-main">Berita Selengkapnya</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h3 class="galeri">Galeri</h3>
+                <div class="garis-galeri"></div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <img class="foto-galeri" src="https://cdn1-production-images-kly.akamaized.net/R17oSA8pk7wUqHoqTtC2J_x7JBY=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3367898/original/038574900_1612405764-timon-klauser-3MAmj1ZKSZA-unsplash.jpg">
+                </div>
+                <div class="col-4">
+                    <img class="foto-galeri" src="https://cdn1-production-images-kly.akamaized.net/R17oSA8pk7wUqHoqTtC2J_x7JBY=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3367898/original/038574900_1612405764-timon-klauser-3MAmj1ZKSZA-unsplash.jpg">
+                </div>
+                <div class="col-4">
+                    <img class="foto-galeri" src="https://cdn1-production-images-kly.akamaized.net/R17oSA8pk7wUqHoqTtC2J_x7JBY=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3367898/original/038574900_1612405764-timon-klauser-3MAmj1ZKSZA-unsplash.jpg">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <video width="500" controls>
+                    <source src="https://youtu.be/WvZ3fCd-GNU" type="video" />
+            </div>
+            <div class="col-6">
+                <video width="500" controls>
+                    <source src="https://youtu.be/WvZ3fCd-GNU" type="video" />
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <h4 class="footer">2022 UKM Bidang Seni, Politeknik Negeri Lampung</h4>
+        </div>
+    </div>
+    </div>
+    </div>
+
+</body>
+
+</html>
---
+++ app/Views/profile.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="profile.css">
+
+</head>
+
+<body>
+    <div class="container-main">
+        <div class="row">
+            <div class="col-6">
+                <img class="ukuran-logo" src="http://1.bp.blogspot.com/-ZVv0pkxHftc/VKlBf-ZrnXI/AAAAAAAABOk/C3vVyWcyKjg/s1600/ukm-bs%2Bpolinela.png">
+            </div>
+            <div class="col-6">
+                <nav class="navbar navbar-expand-lg">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse " id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a href="/" class="nav-link tulisan-menu active" aria-current="page">Beranda</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tulisan-menu">Profil</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tulisan-menu">Berita</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tulisan-menu disabled">Galeri</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tulisan-menu menu-kontak">Kontak</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div class="container-title2">
+                    <div class="container-title">
+                        <div class="container-text">
+                            <h1 class="title">Hallo, Selamat Datang Di UKM Bidang Seni</h1>
+                            <p class="content">Ini isinya...</p>
+                            <input type="text" class="search-input" placeholder="cari">
+                        </div>
+                    </div>
+                    <img class="gambar" src="https://cdn3.vectorstock.com/i/1000x1000/76/32/doodle-seamless-background-school-pattern-vector-21587632.jpg">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h3 class="visi-misi">Visi Misi</h3>
+                <div class="garis"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8">
+                <h5 class="text-visi">Visi</h5>
+                <p>Menjadi politeknik yang bermutu, inovatif, dan unggul dalam ilmu pengetahuan dan teknologi terapan</p>
+            </div>
+            <div class="col-4">
+
+            </div>
+        </div>
